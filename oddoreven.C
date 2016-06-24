ca2758636391cdbf15c3d23ca9bcb585c48d7393
venkatesh2511 <venkat251195@gmail.com>
2016-06-24 19:00:32 +0530

    Create oddoreven.C

---
+++ oddoreven.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int num;
+	printf("\n enter the number");
+	scanf("%d",&num);
+	
+	if(num%2==0)
+	{
+		printf("the number is even");
+	}
+	else
+		printf("the number is odd");
+}
+		
+	
+
