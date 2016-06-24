2424b0a01cf18406676060d45c64077b1f4d0972
venkatesh2511 <venkat251195@gmail.com>
2016-06-24 20:29:22 +0530

    Create vowel.C

---
+++ vowel.C
@@ -0,0 +1,15 @@
+#include<stdio.H>
+#include<conio.h>
+int main()
+{
+	char c;
+	printf("\n Enter the character\n");
+	scanf("%s",&c);
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+	{
+		printf("\n the character is vowel\n");
+	}
+	else
+		printf("\n the character is consanant\n");
+}	 
+
